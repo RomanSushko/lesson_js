4546bf65dcba3a9feb867df99a61af58f6a54efb
Roman <slimbrazil10@gmail.com>
2021-03-13 17:11:28 +0200

    lesson_js

---
+++ js/script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+let number = 5;
+console.log(number);
+
+let und;
+console.log(und);
+
+const obj = {
+  name: "Roman",
+  age: 25,
+  isMarried: false,
+};
+
+console.log(obj.name);
+
+let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+console.log(arr[1]);
+
+const work = {
+  name: "metro",
+  time: 7,
+};
+
+console.log(work.name);
+
+let breek = ["Roman", 30, "line.jpg"];
+console.log(breek[1]);
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как выше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(answers);
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/true`);
+
+// const user = 'Roman';
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + '-object');
+
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 === 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(!isChecked && !isClose);
+
