d122d9fe405c608de28e3db81123481d8928f080
Roman <slimbrazil10@gmail.com>
2021-03-13 22:08:36 +0200

    lesson_12

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 "use strict";
 
-let number = 5;
-console.log(number);
+// let number = 5;
+// console.log(number);
 
-let und;
-console.log(und);
+// let und;
+// console.log(und);
 
-const obj = {
-  name: "Roman",
-  age: 25,
-  isMarried: false,
-};
+// const obj = {
+//   name: "Roman",
+//   age: 25,
+//   isMarried: false,
+// };
 
-console.log(obj.name);
+// console.log(obj.name);
 
-let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
-console.log(arr[1]);
+// let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(arr[1]);
 
-const work = {
-  name: "metro",
-  time: 7,
-};
+// const work = {
+//   name: "metro",
+//   time: 7,
+// };
 
-console.log(work.name);
+// console.log(work.name);
 
-let breek = ["Roman", 30, "line.jpg"];
-console.log(breek[1]);
+// let breek = ["Roman", 30, "line.jpg"];
+// console.log(breek[1]);
 
 // alert('hello');
 
@@ -52,23 +52,68 @@ console.log(breek[1]);
 
 // alert(`Привет, ${user}`);
 
-console.log('arr' + '-object');
+// console.log('arr' + '-object');
 
-console.log(4 + '5');
+// console.log(4 + '5');
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// --decr;
+// // ++incr;
+// // --decr;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
-console.log(2*4 === 8);
+// console.log(2*4 === 8);
 
-const isChecked = false,
-      isClose = false;
+// const isChecked = false,
+//       isClose = false;
 
-console.log(!isChecked && !isClose);
+// console.log(!isChecked && !isClose);
 
+
+/* Задание на урок 12:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+const namberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+   count: namberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+// урок окончен
